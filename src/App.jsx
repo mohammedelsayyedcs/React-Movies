a54eecfce339ac7c1517f6e4c88990d21d82b23f
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-04 16:27:02 +0200

    Import Bootstrap in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 
 function App() {
 
