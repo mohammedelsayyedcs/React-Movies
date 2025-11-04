110dcb84f3e30783f2d7c22fc097dc1dbe65023c
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-04 18:08:43 +0200

    Get 1st Page of Now Playing Movies

--- src/Components/NowPlaying/NowPlaying.jsx
+++ src/Components/NowPlaying/NowPlaying.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NowPlaying.css'
+import { getApi } from '../../AxiosInstance';
 
 export default function NowPlaying() {
+    const [moviesObj, setMoviesObj] = useState({});
+    const url = "/now_playing?language=en-US&page=1";
+
+    const getMoviesObject = async (url) => {
+        try {
+            const res = await getApi.get(url);
+            setMoviesObj(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    useEffect(() => {
+        getMoviesObject(url);
+    }, [])
+
     return (
         <div>
-            <h3>Now Playing</h3>
+            <ul>
+                {moviesObj.results?.map((item) => {
+                    return <li key={item.id}>{item.title}</li>
+                })}
+            </ul>
         </div>
     )
 }
