33f8ef9d11f03d81d6c1009eeb1bf84aafe7ed59
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-04 16:36:16 +0200

    Build Project Outline

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import { Route, Routes } from 'react-router-dom'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home.jsx'
 
 function App() {
 
   return (
     <>
+      <Header />
+      <Home />
+      <Routes>
+        <Route path='' element='' />
+      </Routes>
 
+      <Footer />
     </>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Home.css'
+import Movies from '../Movies/Movies'
+import Sidebar from '../Sidebar/Sidebar'
 
 export default function Home() {
     return (
-        <div className='body'>
-            <h1>Home</h1>
+        <div className='body row container-fluid'>
+            <div className='col-2'>
+                <Sidebar/>
+            </div>
+            <div className='col-10'>
+                <Movies />
+            </div>
         </div>
     )
 }
