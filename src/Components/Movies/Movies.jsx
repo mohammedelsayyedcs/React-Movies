a371ffecc436001f19b33cc1d953f2045120454f
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-05 12:00:20 +0200

    Implement Popular.jsx

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -3,9 +3,8 @@ import './Movies.css'
 import { Outlet } from 'react-router-dom'
 
 export default function Movies() {
-    return (
-        <div>
-            {/* <Outlet /> */}
-        </div>
+    return(
+        <>
+        </>
     )
 }
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.css'
+import { getApi } from '../../AxiosInstance';
+import Movie from '../Movie/Movie';
 
 export default function Popular() {
-  return (
-    <div>
-        <h3>Popular</h3>
-    </div>
-  )
+    const [moviesObj, setMoviesObj] = useState({});
+    const url = "/popular?language=en-US&page=1";
+
+    const getMovies = async (url) => {
+        try {
+            const res = await getApi.get(url);
+            setMoviesObj(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Execute in first page load
+    useEffect(() => {
+        getMovies(url);
+    }, [])
+
+    return (
+        <div className='row container-fluid'>
+            {
+                moviesObj.results?.map((item) => {
+                    return <Movie key={item.id} movie={item} />
+                })
+            }
+        </div>
+    )
 }
