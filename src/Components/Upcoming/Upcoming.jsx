18efec31ec1a69faa95f4b20f3901e5bdac5e1fa
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-05 12:08:29 +0200

    Implement Upcoming.jsx

--- src/Components/Upcoming/Upcoming.jsx
+++ src/Components/Upcoming/Upcoming.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Upcoming.css'
+import { getApi } from '../../AxiosInstance'
+import Movie from '../Movie/Movie'
 
 export default function Upcoming() {
+    // Store Movies Object in moviesObj
+    const [moviesObj, setMoviesObj] = useState({})
+    const url = "/upcoming?language=en-US&page=1"
+
+    // Fetch Upcoming Movies
+    const getMovies = async (url) => {
+        try {
+            const res = await getApi.get(url);
+            setMoviesObj(res.data);           
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Call getMovies one time in first page load
+    useEffect(()=>{
+        getMovies(url)
+    },[])
+
     return (
-        <div>
-            <h3>Upcoming</h3>
+        <div className='row container-fluid`'>
+            {
+                moviesObj.results?.map((item)=>{
+                    return <Movie key={item.id} movie={item}/>
+                })
+            }
         </div>
     )
 }
