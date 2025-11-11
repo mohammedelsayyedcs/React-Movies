a7870a86f268b35ce5ef0fff8ebe664726a33fe2
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-11 11:03:16 +0200

    Let TopRated.jsx Use Redux to Set and Get Movies Object

--- src/Components/TopRated/TopRated.jsx
+++ src/Components/TopRated/TopRated.jsx
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from 'react'
 import './TopRated.css'
 import { getMoviesListApi } from '../../AxiosInstance'
 import Movie from '../Movie/Movie';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMoviesObj } from '../../ReduxTK/moviesSlice';
 
 export default function TopRated() {
-    const [moviesObj, setMoviesObj] = useState({});
+    // Redux
+    const moviesObj = useSelector(state => state.movies.moviesObj);
+    const dispatch = useDispatch();
+
+    // const [moviesObj, setMoviesObj] = useState({});
     const url = "/top_rated?language=en-US&page=1";
 
     const getMovies = async (url) => {
         try {
             const res = await getMoviesListApi.get(url);
-            setMoviesObj(res.data);
+            // setMoviesObj(res.data);
+            dispatch(setMoviesObj(res.data));
         } catch (error) {
             console.log(error);
         }
